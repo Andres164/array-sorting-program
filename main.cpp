8c41ac37f38fd7278e0b1753dc653df8ee858ed1
Andres Antonio Castro Beltran <andres435@gmail.com>
2021-05-21 19:25:31 -0600

    Modified main.cpp, added a class named arraySorters, here are going to be all the array sorters in form of methods, also some other methods that will be needed for that. \nTODO: finish the swap elements of the array and finish selectionSort method.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,40 @@
 
 using namespace std;
 
+class arraySorters
+{
+public:
+    arraySorters();
+    void selectionSort(int arrayToSort[])
+    {
+        int lengthOfArrayToSort = lengthOfArray(arrayToSort);
+        for(int i = 0; i < lengthOfArrayToSort; i++)
+        {
+            int lesserNumber = arrayToSort[i];
+            for(int j = i + 1; j < lengthOfArrayToSort; j++)
+            {
+                if(arrayToSort[j] < lesserNumber)
+                    lesserNumber = arrayToSort[j];
+            }
+
+        }
+    }
+
+private:
+    int lengthOfArray(int arr[])
+    {
+        return (sizeof(arr)/sizeof(arr[0]));
+    }
+
+    void swapElementsOfArray(int index1, int index2, int* arr[])
+    {
+        int temp;
+        temp = *arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2]* = temp;
+    }
+};
+
 int main()
 {
 
