a14689d4e11c17e8a62ba6dd77efe0737dab4bce
Andres Antonio Castro Beltran <andres435@gmail.com>
2021-05-25 10:32:36 -0600

    Finished the swapElementsOfArray method, selection sort method is working. TODO: modify the array passed as argument to the method

--- main.cpp
+++ main.cpp
@@ -5,40 +5,40 @@ using namespace std;
 class arraySorters
 {
 public:
-    arraySorters();
-    void selectionSort(int arrayToSort[])
+    void selectionSort(int arrayToSort[], int lengthOfArrayToSort)
     {
-        int lengthOfArrayToSort = lengthOfArray(arrayToSort);
         for(int i = 0; i < lengthOfArrayToSort; i++)
         {
-            int lesserNumber = arrayToSort[i];
+            int lesserNumberIndex = i;
             for(int j = i + 1; j < lengthOfArrayToSort; j++)
             {
-                if(arrayToSort[j] < lesserNumber)
-                    lesserNumber = arrayToSort[j];
+                if(arrayToSort[j] < arrayToSort[lesserNumberIndex])
+                    lesserNumberIndex = j;
             }
-
+            swapElementsOfArray(&arrayToSort[i], &arrayToSort[lesserNumberIndex]);
         }
+
+        for(int i = 0; i < lengthOfArrayToSort; i++)
+            cout << arrayToSort[i] << " ";
     }
 
 private:
-    int lengthOfArray(int arr[])
+    void swapElementsOfArray(int* ptr_element1, int* ptr_element2)
     {
-        return (sizeof(arr)/sizeof(arr[0]));
-    }
+        int temp = *ptr_element1;
+        *ptr_element1 = *ptr_element2;
+        *ptr_element2 = temp;
 
-    void swapElementsOfArray(int index1, int index2, int* arr[])
-    {
-        int temp;
-        temp = *arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2]* = temp;
     }
 };
 
 int main()
 {
+    int lengthOfArray = 6;
+    int arr1[lengthOfArray] = {2,1,3,4,6,5};
 
+    arraySorters myObjct;
+    myObjct.selectionSort(arr1, lengthOfArray);
 
     return 0;
 }
